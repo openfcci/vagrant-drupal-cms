b9ebad125678998a86b897909cf8b803b3913c91
Brendan Tobolaski <brendan@tobolaski.com>
2013-12-02 23:46:02 -0600

    Prevent the database dump from being downloaded more than once a day

--- chef/cookbooks/drupal-dev/files/default/db_util.sh
+++ chef/cookbooks/drupal-dev/files/default/db_util.sh
@@ -6,5 +6,9 @@ dbname=`php lib/get_db_setting.php database`
 username=`php lib/get_db_setting.php username`
 password=`php lib/get_db_setting.php password`
 echo "DROP DATABASE $dbname; CREATE DATABASE $dbname" | mysql -h $hostname -u $username -p$password $dbname
-wget $1
+if [ ! -e $1 ]
+    then
+    rm -f *.sql.gz
+    wget http://dbdump.fccinteractive.com/$1
+fi
 gunzip -c *.sql.gz | mysql -h $hostname -u $username -p$password $dbname
